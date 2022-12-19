e9967bf7f934b62c87dcd9914115a28c7ea6cce1
Mario Villasante <mario.villasante@islas.org.mx>
2022-12-19 14:41:28 -0800

    🏹👾 Catch mutant

--- jinja_render/jinja_render.py
+++ jinja_render/jinja_render.py
@@ -3,7 +3,7 @@ import json
 from jinja2 import Environment, FileSystemLoader
 
 
-def write_tex(report_name: str, summary_path="tests/data/results.json"):
+def write_tex(report_name: str, summary_path=None):
     rendered_report = get_rendered_report(report_name, summary_path)
     with open(f"reports/{report_name}.tex", "w") as report_tex:
         report_tex.writelines(rendered_report)
@@ -24,8 +24,10 @@ def get_jinja_latex():
     return latex_jinja_env
 
 
-def get_rendered_report(report_name, path):
-    effort_summary = load_json(path)
+def get_rendered_report(report_name, path = None):
+    effort_summary = {}
+    if path is not None:
+        effort_summary = load_json(path)
     latex_jinja_env = get_jinja_latex()
     template = latex_jinja_env.get_template(f"reports/templates/{report_name}.tex")
     return template.render(effort_summary)
--- tests/test_jinja_render.py
+++ tests/test_jinja_render.py
@@ -7,7 +7,6 @@ from jinja_render import (
 )
 
 
-expected_hash = "239431b70e3e6f770c21cef4ee6d22ee"
 report_name = "paper"
 summary_path = "tests/data/results.json"
 
@@ -21,11 +20,18 @@ def test_write_rendered_report():
 
 
 def test_rendered_report():
-    obtained_hash = _get_hash_from_tex_file()
+    expected_hash = "239431b70e3e6f770c21cef4ee6d22ee"
+    report_name = "paper"
+    obtained_hash = _get_hash_from_tex_file(report_name)
     assert obtained_hash == expected_hash, "El hash del archivo reports/paper.tex"
+    
+    expected_hash = "74b0b5fed1926ff3c6f641553d32866f"
+    report_name_without_summary = "paper_without_summary"
+    obtained_hash = _get_hash_from_tex_file(report_name_without_summary)
+    assert obtained_hash == expected_hash, "El hash del archivo reports/paper_without_summary.tex"
 
 
-def _get_hash_from_tex_file():
+def _get_hash_from_tex_file(report_name):
     rendered_report = get_rendered_report(report_name, summary_path)
     report_content = rendered_report.encode("utf-8")
     obtained_hash = hashlib.md5(report_content).hexdigest()
