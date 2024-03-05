b80ce691d72f4397c0bf0e8aa05e69138188aaf3
Nepo Rojas • GECI <braulio.rojas@islas.org.mx>
2024-03-05 14:49:54 -0800

    ✔️ Update hash

--- tests/test_top_info.py
+++ tests/test_top_info.py
@@ -1,6 +1,6 @@
 from jinja_render import get_repo_and_hash, get_hash_from_branch
 
-hash_repo = "31c31a791e017550f11986d5a766ddef8803fbd1"
+hash_repo = "be0a1f7b95249cbce2d224d9f09fe52fbad8d8fe"
 
 
 def test_get_repo_and_hash():
