6c9c51169e34dfa12cbf18ae032279b3db40c709
Guillermo Olvera <guillermo.olvera@islas.org.mx>
2024-03-07 12:46:16 -0800

    🏹👾 Erradicate last mutants

--- jinja_render/get_info_top.py
+++ jinja_render/get_info_top.py
@@ -1,6 +1,12 @@
 import os
 
 
+def get_repo_and_hash(branch):
+    hash = get_hash_from_branch(branch)
+    name = get_repo_name()
+    return {"repo": name, "hash": hash}
+
+
 def get_hash_from_branch(branch):
     command = "git config --global --add safe.directory /workdir"
     run_command(command)
@@ -9,21 +15,14 @@ def get_hash_from_branch(branch):
     return output.split("\n")[0]
 
 
-def run_command(command):
-    exit_code = os.system(command)
-    assert exit_code == 0
-
-
-
 def get_repo_name():
     command = "git config --global --add safe.directory /workdir"
     run_command(command)
     stream = os.popen("basename $(git remote get-url origin)")
     output = stream.read()
-    return output.split(".")[0].strip("\n")
+    return output.split(".")[0]
 
 
-def get_repo_and_hash(branch="develop"):
-    hash = get_hash_from_branch(branch=branch)
-    name = get_repo_name()
-    return {"repo": name, "hash": hash}
+def run_command(command):
+    exit_code = os.system(command)
+    assert exit_code == 0
